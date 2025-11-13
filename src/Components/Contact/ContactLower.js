90641370ce961ad870d65384c9e0b00081f3a2f9
Neetika Goel <neetikagoel786@gmail.com>
2025-11-14 01:47:58 +0530

    Updated the content!

--- src/Components/Contact/ContactLower.js
+++ src/Components/Contact/ContactLower.js
@@ -10,7 +10,7 @@ const ContactLower = () => {
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 gap-x-10 w-5/6 mb-10 md:mb-0">
                <img className="object-cover object-center rounded" alt="hero" src="https://thechefkart.com/_next/image?url=%2FImage.png&w=1920&q=75"/>
-               <p className="mb-8 leading-relaxed font-bold text-red-950">ChefKart is a platform that connects home chefs and households seamlessly by building an ecosystem. Our professionals are handpicked to serve you hygienic and wholesome meals catered to your taste!</p>
+               <p className="mb-8 leading-relaxed font-bold text-red-950">ChefKart is a platform that connects home Chefs and Households seamlessly by building an Ecosystem. Our professionals are handpicked to serve you hygienic and wholesome meals catered to your taste!</p>
           </div>
           {/* <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"> */}
                {/* <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out */}
